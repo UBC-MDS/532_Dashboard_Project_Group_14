901c414f5e0285172451f4ae3d912eb3595daad5
deepcl <hkcmul@gmail.com>
2021-02-06 11:04:21 -0800

    test 3

--- src/app_new.py
+++ src/app_new.py
@@ -8,7 +8,7 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 import base64
 
-# deploymment test
+# deploymment test v2
 # Read Data - Don't Change the Path
 df = pd.read_csv(r"data/Processed/HR_employee_Attrition_editted.csv")
 # Convert variables to categoriccal and reordering by label.
